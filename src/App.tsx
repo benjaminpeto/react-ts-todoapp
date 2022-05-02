d8793ea11eafeb2fccef444503c0901e0e827559
Benji <benjaminpeto23@gmail.com>
2022-05-02 16:36:12 +0200

    initiated model.ts for the interface of Todo, made function for handling input and submission, issue as a bug: on inputfield Enter-key, doesn't clear the dark effect after submitting input

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,30 @@ import React, { useState } from 'react';
 import './App.css';
 
 import { InputField } from './components/inputField/InputField';
+import { Todo } from './model';
+
 
 const App: React.FC = () => {
 
   const [todo, setTodo] = useState<string>('');
+  const [todos, setTodos] = useState<Todo[]>([]); // All of the todos
 
-  console.log(todo);
+  const handleAddTodo = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if(todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo('');
+    }
+  };
+
+  console.log(todos);
 
   return (
     <div className="App">
       <h1 className="heading">To Do App</h1>
-      <InputField todo={todo} setTodo={setTodo}/>
+      <InputField todo={todo} setTodo={setTodo} handleAddTodo={handleAddTodo} />
+      {/* <TodoList /> */}
     </div>
   );
 }
--- src/components/inputField/InputField.tsx
+++ src/components/inputField/InputField.tsx
@@ -4,11 +4,12 @@ import './InputField.style.css';
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>; //What the heck, at least VSCode helps to figure out what types I need
+  handleAddTodo: (e: React.FormEvent) => void;
 }
 
-export const InputField = ({ todo, setTodo }: Props) => {
+export const InputField = ({ todo, setTodo,handleAddTodo }: Props) => {
   return (
-    <form className='input'>
+    <form className='input' onSubmit={handleAddTodo}>
       <input
         type="input"
         placeholder='Write your tasks here...'
