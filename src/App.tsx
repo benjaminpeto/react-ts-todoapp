754d91104ab880a377406f0c6a0b90ea6c9dd59e
Benji <benjaminpeto23@gmail.com>
2022-05-02 15:43:13 +0200

    added state for input value by using typescript :D

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import { InputField } from './components/inputField/InputField';
 
 const App: React.FC = () => {
 
-  const [todo, setTodo] = useState("");
+  const [todo, setTodo] = useState<string>('');
+
+  console.log(todo);
 
   return (
     <div className="App">
       <h1 className="heading">To Do App</h1>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo}/>
     </div>
   );
 }
--- src/components/inputField/InputField.tsx
+++ src/components/inputField/InputField.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './InputField.style.css';
 
-export const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>; //What the heck, at least VSCode helps to figure out what types I need
+}
+
+export const InputField = ({ todo, setTodo }: Props) => {
   return (
     <form className='input'>
-      <input type="input" placeholder='Write your tasks here...' className='input-field'></input>
+      <input
+        type="input"
+        placeholder='Write your tasks here...'
+        className='input-field'
+        value={todo}
+        onChange={(event) => setTodo(event.target.value)}>
+      </input>
       <button className='input-btn' type="submit">Enter</button>
     </form>
   )
